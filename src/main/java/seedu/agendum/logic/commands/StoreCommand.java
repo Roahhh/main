34bc1d26a2d55606fbe108b29f4026de387d2384
Nurbs\Justin <justintay123@gmail.com>
2016-10-10 00:03:11 +0800

    Update StoreCommand class

--- src/main/java/seedu/agendum/logic/commands/StoreCommand.java
+++ src/main/java/seedu/agendum/logic/commands/StoreCommand.java
@@ -1,5 +1,10 @@
 package seedu.agendum.logic.commands;
 
+import java.io.IOException;
+
+import seedu.agendum.commons.core.Config;
+import seedu.agendum.commons.util.ConfigUtil;
+
 /**
  * Allow the user to specify a folder as the data storage location
  */
@@ -8,22 +13,25 @@ public class StoreCommand extends Command {
     public static final String COMMAND_WORD = "store";
     public static final String MESSAGE_SUCCESS = "New data storage location: %1$s";
     public static final String MESSAGE_LOCATION_DOES_NOT_EXIT = "The specified location does not exit.";
+    
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Specify a data storage location. \n"
             + "Parameters: FILE_PATH\n" 
             + "Example: " + COMMAND_WORD 
             + "C:/agendum";
-    private final String newLocation;
+    private final String newToDoListFilePath;
 
     public StoreCommand(String location) {
-        newLocation = location;
+        newToDoListFilePath = location;
     }
 
     @Override
     public CommandResult execute() {
-        assert model != null;
+        assert newToDoListFilePath != null;
+        
         try {
-            return new CommandResult(String.format(MESSAGE_SUCCESS, newLocation));
-        } catch (Exception e) {
+            ConfigUtil.saveToDoListFilePathInConfig(Config.DEFAULT_CONFIG_FILE, newToDoListFilePath);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, newToDoListFilePath));
+        } catch (IOException e) {
             return new CommandResult(MESSAGE_LOCATION_DOES_NOT_EXIT);
         }
     }
