7082dd50a963d689e4558687d72e4467352ff96f
Fan Weiguang <lahalaha90@gmail.com>
2016-10-12 00:15:41 +0800

    Change icon to agendum

--- src/main/java/seedu/agendum/ui/MainWindow.java
+++ src/main/java/seedu/agendum/ui/MainWindow.java
@@ -21,7 +21,7 @@ import seedu.agendum.model.task.ReadOnlyTask;
  */
 public class MainWindow extends UiPart {
 
-    private static final String ICON = "/images/address_book_32.png";
+    private static final String ICON = "/images/agendum_icon.png";
     private static final String FXML = "MainWindow.fxml";
     public static final int MIN_HEIGHT = 600;
     public static final int MIN_WIDTH = 450;
