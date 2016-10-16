6a1c4283dcd95131d6885dd17ca425c3b5c6d062
Vishnu Prem <vishnu@vishnuprem.com>
2016-10-16 19:30:46 -0400

    Initial work for test to work

--- src/test/java/seedu/agendum/logic/DateTimerParserTest.java
+++ src/test/java/seedu/agendum/logic/DateTimerParserTest.java
@@ -11,13 +11,25 @@ import static org.junit.Assert.assertTrue;
 
 public class DateTimerParserTest {
 
-    private void assertDates(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+    private void assertAbsoluteDates(LocalDateTime dateTime1, LocalDateTime dateTime2) {
         assertEquals(dateTime1, dateTime2);
     }
+
+    private void assertRelativeDates(LocalDateTime dateTime1, int seconds) {
+
+    }
+
+
 
     @Test
     public void simpleDateTest() throws Exception {
         Optional<LocalDateTime> t = DateTimeParser.parseString("2016/01/01 00:00");
-        assertDates(t.get(), LocalDateTime.of(2016,1,1,0,0));
+        assertAbsoluteDates(t.get(), LocalDateTime.of(2016,1,1,0,0));
     }
+
+    public void relativeDateTest() throws Exception {
+        // Mock the Date object here?
+        Optional<LocalDateTime> t = DateTimeParser.parseString("next Monday");
+    }
+
 }
