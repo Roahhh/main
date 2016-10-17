985194d7992b56fa86f9183bb738b76be15fa86d
Vishnu Prem <vishnu@vishnuprem.com>
2016-10-17 15:59:06 -0700

    Add DateTimeParserTests

--- src/test/java/seedu/agendum/logic/DateTimeParserTest.java
+++ src/test/java/seedu/agendum/logic/DateTimeParserTest.java
@@ -3,6 +3,7 @@ package seedu.agendum.logic;
 import org.junit.Test;
 import seedu.agendum.logic.parser.DateTimeParser;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -11,25 +12,45 @@ import static org.junit.Assert.assertTrue;
 
 public class DateTimeParserTest {
 
-    private void assertAbsoluteDates(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+    private void assertSameDateAndTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
         assertEquals(dateTime1, dateTime2);
     }
 
-    private void assertRelativeDates(LocalDateTime dateTime1, int seconds) {
-
+    private void assertSameDate(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        LocalDateTime diff = dateTime1.minusHours(dateTime2.getHour()).minusMinutes(dateTime2.getMinute()).minusSeconds(dateTime2.getSecond());
+        assertEquals(dateTime1, diff);
     }
-
-
 
     @Test
-    public void simpleDateTest() throws Exception {
-        Optional<LocalDateTime> t = DateTimeParser.parseString("2016/01/01 00:00");
-        assertAbsoluteDates(t.get(), LocalDateTime.of(2016,1,1,0,0));
+    public void simpleAbsoluteDateTest() throws Exception {
+        Optional<LocalDateTime> t = DateTimeParser.parseString("2016/01/01");
+        assertSameDate(t.get(), LocalDateTime.of(2016,1,1,0,0));
     }
 
-    public void relativeDateTest() throws Exception {
-        // Mock the Date object here?
-        Optional<LocalDateTime> t = DateTimeParser.parseString("next Monday");
+    @Test
+    public void simpleAbsoluteDateTimeTest() throws Exception {
+        Optional<LocalDateTime> t = DateTimeParser.parseString("2016/01/01 01:00");
+        assertSameDateAndTime(t.get(), LocalDateTime.of(2016,1,1,1,0));
     }
+
+    @Test
+    public void simpleAbsoluteDateTimeTestPM() throws Exception {
+        Optional<LocalDateTime> t = DateTimeParser.parseString("2016/01/01 3pm");
+        assertSameDateAndTime(t.get(), LocalDateTime.of(2016,1,1,15,0));
+    }
+
+    @Test
+    public void verboseAbsoluteDateTest() throws Exception {
+        Optional<LocalDateTime> t = DateTimeParser.parseString("january 10 2017");
+        assertSameDate(t.get(), LocalDateTime.of(2017,1,10,0,0));
+    }
+
+    @Test
+    public void verboseAbsoluteDateTimeTest() throws Exception {
+        Optional<LocalDateTime> t = DateTimeParser.parseString("january 10 2017 5:15pm");
+        assertSameDateAndTime(t.get(), LocalDateTime.of(2017,1,10,17,15));
+    }
+
+    // TODO: Missing relative date time tests
 
 }
