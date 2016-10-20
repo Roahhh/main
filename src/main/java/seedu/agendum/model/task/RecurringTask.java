328483e5f03bb86af63ac3cb0c20e6d82340f199
Fan Weiguang <lahalaha90@gmail.com>
2016-10-20 17:17:00 +0800

    Create recurring task as child class of task

---
+++ src/main/java/seedu/agendum/model/task/RecurringTask.java
@@ -0,0 +1,97 @@
+package seedu.agendum.model.task;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import seedu.agendum.logic.parser.DateTimeParser;
+
+public class RecurringTask extends Task {
+
+    private String period;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    
+    private childRecurringTask child = null;
+    
+    public RecurringTask(Name name, Optional<LocalDateTime> startDateTime, 
+            Optional<LocalDateTime> endDateTime, String period) {
+        super(name, startDateTime, endDateTime);
+        this.startDateTime = startDateTime.orElse(null);
+        this.endDateTime = endDateTime.orElse(null);
+        this.period = period;
+        System.out.println("period: " + period);
+    }
+    
+    public RecurringTask(Name name, Optional<LocalDateTime> endDateTime, String period) {
+        super(name, endDateTime);
+        this.startDateTime = null;
+        this.endDateTime = endDateTime.orElse(null);
+        this.period = period;
+        System.out.println("period: " + period);
+    }
+    
+    public RecurringTask(RecurringTask task) {
+        super(task);
+    }
+    
+    public void setNextDateTime() {
+        if(this.startDateTime != null) {
+            this.startDateTime = DateTimeParser.parseString(period + " from " + this.startDateTime.toString()).get();
+        } 
+        this.endDateTime = DateTimeParser.parseString(period + " from " + this.endDateTime.toString()).get();
+    }
+    
+    public void setPreviousDateTime() {
+        if(this.startDateTime != null) {
+            this.startDateTime = DateTimeParser.parseString(period + " before " + this.startDateTime.toString()).get();
+        } 
+        this.endDateTime = DateTimeParser.parseString(period + " before " + this.endDateTime.toString()).get();
+    }
+    
+    private childRecurringTask setChild() {
+        this.child = new childRecurringTask(this);
+        return this.child;
+    }
+    
+    @Override
+    public Optional<LocalDateTime> getStartDateTime() {
+        return Optional.ofNullable(this.startDateTime);
+    }
+
+    @Override
+    public Optional<LocalDateTime> getEndDateTime() {
+        return Optional.ofNullable(this.endDateTime);
+    }
+
+    @Override
+    public childRecurringTask getChild() {
+        System.out.println("exeuted in recurringTask class");
+        if(this.child == null) {
+            System.out.println("child is null");
+            this.setChild();
+            this.setNextDateTime();
+        } else {
+            this.setPreviousDateTime();
+        }
+        return this.child;
+    }
+    
+    @Override
+    public RecurringTask getParent() {
+        return null;
+    }
+    
+    @Override
+    public boolean isRecurring() {
+        return true;
+    }
+    
+    @Override
+    public boolean isCompleted() {
+        return false;
+    }
+    
+    @Override
+    public void markAsCompleted() {
+        return;
+    }
+}
