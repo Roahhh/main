94a1805c8cb43fbfa96e44c21929aa9b0af16904
rachx <sim.rachael@hotmail.com>
2016-10-29 22:32:49 +0800

    Update messages in commons core

--- src/main/java/seedu/agendum/commons/core/Messages.java
+++ src/main/java/seedu/agendum/commons/core/Messages.java
@@ -8,8 +8,8 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "We don't recognise this command";
     public static final String MESSAGE_UNKNOWN_COMMAND_WITH_SUGGESTION = "Did you mean '%1$s'?";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT =
-            "We don't understand your command format. Try\n%1$s";
-    public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "Hey, the task index given is invalid";
+            "We don't understand your input. Try\n%1$s";
+    public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "Hey, the task id given is invalid";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed/found!";
 
 }
